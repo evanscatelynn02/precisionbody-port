d582b23829316187d02c53fbb5b32ad5b111a9d3
evanscatelynn02 <evanscatelynn02@gmail.com>
2026-05-14 21:16:26 -0600

    Implement authentication middleware and protected routes

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -65,6 +65,45 @@ const registerUser = async (req, res) => {
   }
 };
 
+
+// LOGIN USER
+const loginUser = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+
+    // Find user
+    const user = await User.findOne({ email });
+
+    // Compare password
+    if (
+      user &&
+      (await bcrypt.compare(password, user.password))
+    ) {
+      res.json({
+        _id: user._id,
+        firstName: user.firstName,
+        email: user.email,
+        token: generateToken(user._id),
+      });
+    } else {
+      res.status(401).json({
+        message: "Invalid email or password",
+      });
+    }
+  } catch (error) {
+    res.status(500).json({
+      message: error.message,
+    });
+  }
+};
+
+// GET USER PROFILE
+const getUserProfile = async (req, res) => {
+  res.json(req.user);
+};
+
 module.exports = {
   registerUser,
+  loginUser,
+  getUserProfile,
 };
--- server/routes/authRoutes.js
+++ server/routes/authRoutes.js
@@ -2,13 +2,23 @@ const express = require("express");
 
 const router = express.Router();
 
+const { protect } = require("../middleware/authMiddleware");
+
 const {
   registerUser,
+  loginUser,
+  getUserProfile,
 } = require("../controllers/authController");
 
 
 // Register route
 router.post("/register", registerUser);
+
+//LOGIN
+router.post("/login", loginUser);
+
+//GET PROFILE
+router.get("/profile", protect, getUserProfile);
 
 
 module.exports = router;
