4b79518b39ec353c1f70f75fac7e61f095d181a1
evanscatelynn02 <evanscatelynn02@gmail.com>
2026-05-19 15:23:50 -0600

    Connect frontend to production backend

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: "https://precisionbody-port-service.onrender.com",
 });
 
 export default API;
